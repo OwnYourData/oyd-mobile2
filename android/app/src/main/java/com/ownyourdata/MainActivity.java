63a93d02259d6e164201fdaff5021e896037f579
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2020-09-30 19:49:00 +0200

    Correct native implementation of react-native-orientation

--- android/app/src/main/java/com/ownyourdata/MainActivity.java
+++ android/app/src/main/java/com/ownyourdata/MainActivity.java
@@ -6,5 +6,19 @@ import com.facebook.react.ReactActivity;
 
 import com.reactnativenavigation.NavigationActivity;
 
+// necessary for react-native-orientation implementation
+import android.content.Intent;
+import android.content.res.Configuration;
+
 public class MainActivity extends NavigationActivity {
+
+  // this method is necessary for react-native-orientation to work
+  // https://www.npmjs.com/package/react-native-orientation
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
+  }
 }
